a902dc4c01d5e3f1f954d6132dc8a04410b63761
Destevaaa <2015081193@ust-ics.mygbiz.com>
2018-12-13 12:04:49 +0800

    Bug Fixes

---
+++ app/src/main/java/darryl/esteva/com/nutritionistapp/aboutUs.java
@@ -0,0 +1,57 @@
+package darryl.esteva.com.nutritionistapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class aboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    public void goToHome(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.homeImageButton)
+        {
+            i = new Intent(this,pageTwo.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToAbout(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.aboutImageButton)
+        {
+            i = new Intent(this,aboutUs.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToSettings(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.settingsImageButton)
+        {
+            i = new Intent(this,settings.class);
+            startActivity(i);
+        }
+    }
+}
--- app/src/main/java/darryl/esteva/com/nutritionistapp/pageTwo.java
+++ app/src/main/java/darryl/esteva/com/nutritionistapp/pageTwo.java
@@ -28,6 +28,49 @@ public class pageTwo extends AppCompatActivity {
         myDb = new DatabaseHelper(this);
     }
 
+
+    public void goToHome(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.homeImageButton)
+        {
+            i = new Intent(this,pageTwo.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToAbout(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.aboutImageButton)
+        {
+            i = new Intent(this,aboutUs.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToSettings(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.settingsImageButton)
+        {
+            i = new Intent(this,settings.class);
+            startActivity(i);
+        }
+    }
+
     
 
     public void page3(View v)
