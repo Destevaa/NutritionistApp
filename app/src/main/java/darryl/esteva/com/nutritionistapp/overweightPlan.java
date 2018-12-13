ecdf7609ea3bd0776e08713f23bc86fc12545c0c
Destevaaa <2015081193@ust-ics.mygbiz.com>
2018-12-13 14:08:34 +0800

    Final UI Fixes

--- app/src/main/java/darryl/esteva/com/nutritionistapp/overweightPlan.java
+++ app/src/main/java/darryl/esteva/com/nutritionistapp/overweightPlan.java
@@ -117,4 +117,46 @@ public class overweightPlan extends AppCompatActivity {
         }
 
     }
+
+    public void goToHome(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.homeImageButton)
+        {
+            i = new Intent(this,pageTwo.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToAbout(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.aboutImageButton)
+        {
+            i = new Intent(this,aboutUs.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToSettings(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.settingsImageButton)
+        {
+            i = new Intent(this,settings.class);
+            startActivity(i);
+        }
+    }
 }
--- app/src/main/java/darryl/esteva/com/nutritionistapp/underweightPlan.java
+++ app/src/main/java/darryl/esteva/com/nutritionistapp/underweightPlan.java
@@ -118,4 +118,46 @@ public class underweightPlan extends AppCompatActivity {
         }
 
     }
+
+    public void goToHome(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.homeImageButton)
+        {
+            i = new Intent(this,pageTwo.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToAbout(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.aboutImageButton)
+        {
+            i = new Intent(this,aboutUs.class);
+            startActivity(i);
+        }
+    }
+
+    public void goToSettings(View v)
+    {
+        //Original Code
+        Intent i = null;
+        Intent chooser = null;
+
+        //passing into another page
+        if(v.getId()==R.id.settingsImageButton)
+        {
+            i = new Intent(this,settings.class);
+            startActivity(i);
+        }
+    }
 }
